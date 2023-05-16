8d27f250abd6f02fa983561562ff334dd7b3b56d
SLAVACOM <slavacom121@gmai.com>
2023-05-16 15:25:44 +0300

    norm subject

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import requests
-def getSubject(sbj,day,num):
+import datetime
+def getSubject(sbj,day,num,data):
     sublist = []
     lenn = len(sbj.get(day).get(num))
     for i in range(lenn):
-        sublist.append(sbj.get(day).get(num)[i].get('sbj'))
+        if (sbj.get(day).get(num)[i].get('dt')>=data):
+            sublist.append(sbj.get(day).get(num)[i].get('sbj')+ " ("+sbj.get(day).get(num)[i].get('type')+")")
     return sublist
-def getTeachers(sbj,day,num):
+def getTeachers(sbj,day,num,data):
     sublist = []
     lenn = len(sbj.get(day).get(num))
     for i in range(lenn):
-        sublist.append(sbj.get(day).get(num)[i].get('teacher'))
+        if (sbj.get(day).get(num)[i].get('dt') >= data):
+            sublist.append(sbj.get(day).get(num)[i].get('teacher'))
     return sublist
-def getData(sbj,day,num):
+def getData(sbj,day,num,data):
     sublist = []
     lenn = len(sbj.get(day).get(num))
     for i in range(lenn):
-        sublist.append(sbj.get(day).get(num)[i].get('dts'))
+        if (sbj.get(day).get(num)[i].get('dt') >= data):
+            sublist.append(sbj.get(day).get(num)[i].get('dts'))
     return sublist
-def getRoomNumbers(sbj,day,num):
+def getRoomNumbers(sbj,day,num,data):
     sublist = []
     lenn = len(sbj.get(day).get(num))
     for j in range(lenn):
         lenn1 = len(sbj.get(day).get(num)[j].get('shortRooms'))
         for i in range(lenn1):
-            sublist.append(sbj.get(day).get(num)[j].get('shortRooms')[i])
+            if (sbj.get(day).get(num)[j].get('dt')>=data):
+                sublist.append(sbj.get(day).get(num)[j].get('shortRooms')[i])
     return sublist
 
 def get_data(num):
+    data = str(datetime.datetime.now())
 
     headers = {
         'authority': 'rasp.dmami.ru',
@@ -42,12 +48,10 @@ def get_data(num):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.3.3.719 Yowser/2.5 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
     }
-
     params = {
         'group': num,
         'session': '0',
     }
-
     response = requests.get('https://rasp.dmami.ru/site/group', params=params, headers=headers).json().get('grid')
 
     for i in range(1,7):
@@ -60,12 +64,13 @@ def get_data(num):
         elif i == 6:print("Суббота")
         for j in range(1,8):
             num = str(j)
-            print("Пары:",getSubject(response,day,num),'Даты:',getData(response,day,num),"Преподы:",getTeachers(response,day,num),'Аудитоия:',getRoomNumbers(response,day,num))
+            if len(getSubject(response,day,num,data))!=0:
+                print("Пары:",getSubject(response,day,num,data),'Даты:',getData(response,day,num,data),"Преподы:",getTeachers(response,day,num,data),'Аудитоия:',getRoomNumbers(response,day,num,data))
         print()
 
 
 def main():
-    get_data('221-375')
+    return
 
 if __name__ == '__main__':
     get_data('221-376')
